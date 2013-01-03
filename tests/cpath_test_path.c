7b9df18b16f20ef70817efe8d3ff9ccdc17ca5ec
Joachim Metz <joachim.metz@gmail.com>
2013-01-03 21:06:05 +0100

    2013 update

--- tests/cpath_test_path.c
+++ tests/cpath_test_path.c
@@ -1,7 +1,7 @@
 /*
  * Cross-platform C path functions library path testing program
  *
- * Copyright (c) 2008-2012, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (c) 2008-2013, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
