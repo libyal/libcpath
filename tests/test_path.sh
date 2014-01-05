f1bc9e01125316e19f234de1790f48ed40073dd2
Joachim Metz <joachim.metz@gmail.com>
2014-01-05 17:53:40 +0100

    2014 update.

--- tests/test_path.sh
+++ tests/test_path.sh
@@ -2,7 +2,7 @@
 #
 # Basic File IO (CPATH) library path testing script
 #
-# Copyright (c) 2008-2013, Joachim Metz <joachim.metz@gmail.com>
+# Copyright (c) 2008-2014, Joachim Metz <joachim.metz@gmail.com>
 #
 # Refer to AUTHORS for acknowledgements.
 #
