e214acddf61688ca2317c12ec4092f19c2458bc4
Joachim Metz <joachim.metz@gmail.com>
2016-08-29 08:26:57 +0200

    Worked on tests.

---
+++ tests/cpath_test_error.c
@@ -0,0 +1,149 @@
+/*
+ * Library error functions test program
+ *
+ * Copyright (C) 2008-2016, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+
+#if defined( HAVE_STDLIB_H ) || defined( WINAPI )
+#include <stdlib.h>
+#endif
+
+#include "cpath_test_libcerror.h"
+#include "cpath_test_libcpath.h"
+#include "cpath_test_macros.h"
+#include "cpath_test_unused.h"
+
+/* Tests the libcpath_error_free function
+ * Returns 1 if successful or 0 if not
+ */
+int cpath_test_error_free(
+     void )
+{
+	/* Test invocation of function only
+	 */
+	libcpath_error_free(
+	 NULL );
+
+	return( 1 );
+}
+
+/* Tests the libcpath_error_fprint function
+ * Returns 1 if successful or 0 if not
+ */
+int cpath_test_error_fprint(
+     void )
+{
+	/* Test invocation of function only
+	 */
+	libcpath_error_fprint(
+	 NULL,
+	 NULL );
+
+	return( 1 );
+}
+
+/* Tests the libcpath_error_sprint function
+ * Returns 1 if successful or 0 if not
+ */
+int cpath_test_error_sprint(
+     void )
+{
+	/* Test invocation of function only
+	 */
+	libcpath_error_sprint(
+	 NULL,
+	 NULL,
+	 0 );
+
+	return( 1 );
+}
+
+/* Tests the libcpath_error_backtrace_fprint function
+ * Returns 1 if successful or 0 if not
+ */
+int cpath_test_error_backtrace_fprint(
+     void )
+{
+	/* Test invocation of function only
+	 */
+	libcpath_error_backtrace_fprint(
+	 NULL,
+	 NULL );
+
+	return( 1 );
+}
+
+/* Tests the libcpath_error_backtrace_sprint function
+ * Returns 1 if successful or 0 if not
+ */
+int cpath_test_error_backtrace_sprint(
+     void )
+{
+	/* Test invocation of function only
+	 */
+	libcpath_error_backtrace_sprint(
+	 NULL,
+	 NULL,
+	 0 );
+
+	return( 1 );
+}
+
+/* The main program
+ */
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+int wmain(
+     int argc CPATH_TEST_ATTRIBUTE_UNUSED,
+     wchar_t * const argv[] CPATH_TEST_ATTRIBUTE_UNUSED )
+#else
+int main(
+     int argc CPATH_TEST_ATTRIBUTE_UNUSED,
+     char * const argv[] CPATH_TEST_ATTRIBUTE_UNUSED )
+#endif
+{
+	CPATH_TEST_UNREFERENCED_PARAMETER( argc )
+	CPATH_TEST_UNREFERENCED_PARAMETER( argv )
+
+	CPATH_TEST_RUN(
+	 "libcpath_error_free",
+	 cpath_test_error_free() )
+
+	CPATH_TEST_RUN(
+	 "libcpath_error_fprint",
+	 cpath_test_error_fprint() )
+
+	CPATH_TEST_RUN(
+	 "libcpath_error_sprint",
+	 cpath_test_error_sprint() )
+
+	CPATH_TEST_RUN(
+	 "libcpath_error_backtrace_fprint",
+	 cpath_test_error_backtrace_fprint() )
+
+	CPATH_TEST_RUN(
+	 "libcpath_error_backtrace_sprint",
+	 cpath_test_error_backtrace_sprint() )
+
+	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
+}
+
