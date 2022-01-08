f800c5081a14ac30f0852f3757db232bcf2a545f
Joachim Metz <joachim.metz@gmail.com>
2022-01-08 07:10:59 +0100

    Applied updates

--- libcpath/libcpath_libcsplit.h
+++ libcpath/libcpath_libcsplit.h
@@ -1,7 +1,7 @@
 /*
  * The internal libcsplit header
  *
- * Copyright (C) 2008-2021, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2022, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -46,7 +46,7 @@
 
 #include <libcsplit.h>
 
-#endif
+#endif /* defined( HAVE_LOCAL_LIBCSPLIT ) */
 
-#endif
+#endif /* !defined( _LIBCPATH_LIBCSPLIT_H ) */
 
