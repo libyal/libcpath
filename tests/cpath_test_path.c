a35eb367be8ffb7b486497c1e0140467fbaddff2
Joachim Metz <joachim.metz@gmail.com>
2023-01-07 07:56:39 +0100

    Applied updates

--- tests/cpath_test_path.c
+++ tests/cpath_test_path.c
@@ -1,7 +1,7 @@
 /*
  * Cross-platform C path functions library path testing program
  *
- * Copyright (C) 2008-2022, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2023, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
