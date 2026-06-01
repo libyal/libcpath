4e4622fb8246ea5b0facee0d85a413df617ba332
Joachim Metz <joachim.metz@gmail.com>
2026-06-01 15:24:11 +0200

    Worked on tests and changes for CI tests

--- libcpath/libcpath_extern.h
+++ libcpath/libcpath_extern.h
@@ -24,6 +24,19 @@
 
 #include <common.h>
 
+#if !defined( __CYGWIN__ ) && !defined( _WIN32 ) && defined( __has_attribute )
+#if __has_attribute( visibility )
+#define LIBCPATH_INTERNAL	__attribute__((visibility("hidden"))) extern
+
+#else
+#define LIBCPATH_INTERNAL	extern
+
+#endif /* __has_attribute( visibility ) */
+#else
+#define LIBCPATH_INTERNAL	extern
+
+#endif /* !defined( __CYGWIN__ ) && !defined( _WIN32 ) && defined( __has_attribute ) */
+
 /* Define HAVE_LOCAL_LIBCPATH for local use of libcpath
  */
 #if !defined( HAVE_LOCAL_LIBCPATH )
@@ -32,7 +45,7 @@
 
 #else
 #define LIBCPATH_EXTERN		/* extern */
-#define LIBCPATH_EXTERN_VARIABLE	extern
+#define LIBCPATH_EXTERN_VARIABLE	LIBCPATH_INTERNAL
 
 #endif /* !defined( HAVE_LOCAL_LIBCPATH ) */
 
